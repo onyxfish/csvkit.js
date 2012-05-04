b2a80453dafa9d247cffe196b02256aabac1922f
Christopher Groskopf <staringmonkey@gmail.com>
2012-05-04 12:59:31 -0500

    Add more tests for basic syntax.

--- test/csvkit.js
+++ test/csvkit.js
@@ -2,6 +2,54 @@ describe("CSVKit.Reader", function() {
     beforeEach(function() {
     });
 
+    it("should parse unquoted data", function() {
+        var reader = new CSVKit.Reader();
+        reader.parse('a,b,c\noneword,two words,three words ?');
+
+        expect(reader.column_names).toEqual(["a", "b", "c"]);
+        expect(reader.rows[0]).toEqual(["oneword", "two words", "three words ?"]);
+    });
+
+    it("should parse fully quoted data", function() {
+        var reader = new CSVKit.Reader();
+        reader.parse('"a",b,"c"\noneword,"two words",three words ?');
+
+        expect(reader.column_names).toEqual(["a", "b", "c"]);
+        expect(reader.rows[0]).toEqual(["oneword", "two words", "three words ?"]);
+    });
+
+    it("should parse partially quoted data", function() {
+        var reader = new CSVKit.Reader();
+        reader.parse('"a","b","c"\n"oneword","two words","three words ?"');
+
+        expect(reader.column_names).toEqual(["a", "b", "c"]);
+        expect(reader.rows[0]).toEqual(["oneword", "two words", "three words ?"]);
+    });
+
+    it("should handle Unix line endings", function() {
+        var reader = new CSVKit.Reader();
+        reader.parse('a,b,c\n1,2,3\n4,5,6');
+
+        expect(reader.column_names).toEqual(["a", "b", "c"]);
+        expect(reader.rows.length).toEqual(2);
+    });
+
+    it("should handle Windows line endings", function() {
+        var reader = new CSVKit.Reader();
+        reader.parse('a,b,c\r\n1,2,3\r\n4,5,6');
+
+        expect(reader.column_names).toEqual(["a", "b", "c"]);
+        expect(reader.rows.length).toEqual(2);
+    });
+
+    it("should handle Mac line endings", function() {
+        var reader = new CSVKit.Reader();
+        reader.parse('a,b,c\r1,2,3\r4,5,6');
+
+        expect(reader.column_names).toEqual(["a", "b", "c"]);
+        expect(reader.rows.length).toEqual(2);
+    });
+
     it("should parse types test data", function() {
         var reader = new CSVKit.Reader();
         reader.parse(MOCK_CSV_DATA.types);
